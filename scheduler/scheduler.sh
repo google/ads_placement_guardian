ac78dd1d99212a5892ec90589235a140a6147a59
Andrei Markin <amarkin@google.com>
2025-05-12 16:15:00 +0400

    Update requirements and docker image
    * Update requirements
    * Bump version of ads_placement_guardian library
    * Update base image in docker file
    * Provide correct host for api for scheduler
    
    Change-Id: Ib24deb445d50ff217f42927abb7de4bb5745a924

--- scheduler/scheduler.sh
+++ scheduler/scheduler.sh
@@ -8,9 +8,10 @@ if [ ! -f $CRON_FILE ]; then
 	/usr/bin/crontab $CRON_FILE
 fi
 set -f
+endpoint="${API_HOST:localhost:5000}/api/tasks"
+echo "task endpoint $endpoint"
 while true; do
-	curl -s -H "Accept: application/json" ${API_HOST:-apg_backend}/api/tasks \
-		-o task_list.json
+	curl -s -H "Accept: application/json" $endpoint -o task_list.json
 	> crontab.txt
 	jq -c '.[]' task_list.json | while read i; do
 		task_id=`echo $i | jq -r ".task_id"`
@@ -31,7 +32,7 @@ while true; do
 				cron="* * /$hours * *"
 			fi
 		fi
-		echo "$cron" "curl -X POST $API_HOST/api/tasks/$task_id:run" >> crontab.txt
+		echo "$cron" "curl -X POST ${endpoint}/$task_id:run" >> crontab.txt
 	done
 	cp -f crontab.txt $CRON_FILE
 	sleep 60
